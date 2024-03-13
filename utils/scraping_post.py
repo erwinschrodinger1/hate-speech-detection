f1bf6d1d61e526e30a8c5459556675dcb98e55cd
Ranjan <lamsalranjan19@gmail.com>
2024-03-13 20:37:02 +0545

    facebook post

--- utils/scraping_post.py
+++ utils/scraping_post.py
@@ -32,3 +32,35 @@ def scrape_youtube_caption(link):
         "title":next(dataset.iterate_items())["videoTitle"],
         "caption": datas
     }
+
+
+from apify_client import ApifyClient
+
+
+def scrape_facebook_group_posts(group_url, results_limit=20):
+   
+    run_input = {
+        "startUrls": [{"url": group_url}],
+        "resultsLimit": results_limit,
+    }
+    
+    # Run the Actor and wait for it to finish
+    run = client.actor("apify/facebook-posts-scraper").call(run_input=run_input)
+    
+    # Initialize an empty list to store the scraped data
+    scraped_data = []
+    
+    # Fetch and append Actor results from the run's dataset
+    dataset = client.dataset(run["defaultDatasetId"])
+    for item in dataset.iterate_items():
+        post_text = item.get("text", "")
+        post_url = item.get("url", "")
+        scraped_data.append({"text": post_text, "url": post_url})
+    
+    return scraped_data
+
+
+# Example usage
+group_url = "https://www.facebook.com/kiranjan.zarry"
+posts = scrape_facebook_group_posts(group_url, results_limit=20)
+print(posts)
